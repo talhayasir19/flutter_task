50d6e733a1e97d3bfc5a0fb46c0bbcccde5d5c9a
Talha Yasir <107345586+talhayasir19@users.noreply.github.com>
2024-11-17 02:26:39 +0500

    Update export.dart

--- lib/core/utils/export.dart
+++ lib/core/utils/export.dart
@@ -3,3 +3,4 @@ export 'navigator_util.dart';
 export 'get_it_util.dart';
 export 'flutter_toast_util.dart';
 export 'firebase_pushnotification_util.dart';
+export 'firebase_pushnotification_util.dart';
